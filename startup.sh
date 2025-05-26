6c62ffee02a35edfe9f3914a433befa6d5e85492
Freddy Cruz <freddycruz5227@gmail.com>
2025-05-26 15:08:11 -0400

    Fixed startup.sh

--- startup.sh
+++ startup.sh
@@ -1,29 +1,30 @@
 #!/bin/bash
 
 # Check if docker is already installed
-if [ command -v docker &> /dev/null ]; then
-  # Downloads docker using the script from the docker website
-  if [ command -v curl &> /dev/null ]; then
+if command -v docker &> /dev/null; then
+  echo "Docker is already installed"
+else
+   # Downloads docker using the script from the docker website
+  if command -v curl &> /dev/null; then
     curl -fsSL https://get.docker.com -o get-docker.sh
-    sh ./get-docker.sh --dry-run
+    sudo sh ./get-docker.sh
+    rm get-docker.sh
     echo "Docker installed"
   else
     echo "Curl is not installed, please install it"
     exit
   fi
-else
- echo "Docker is already installed"
 fi
 
 # Enables and starts docker if systemctl is available, this only works on linux systems that use systemd
-if [ command -v systemctl &> /dev/null ]; then
-  systemctl enable --now docker
+if command -v systemctl &> /dev/null; then
+  sudo systemctl enable --now docker
 fi
 
 if [ ! -f .env ]; then
   cp .env.example .env
 fi
 
-docker compose up -d --build
+sudo docker compose up -d --build
 
 echo "Everything's setup"

15d7d059963056103a469941f42041359727b969
Freddy Cruz <freddycruz5227@gmail.com>
2025-05-26 13:06:27 -0400

    Minor fixes to compose and startup.sh

--- startup.sh
+++ startup.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 
 # Check if docker is already installed
-if command -v docker > /dev/null 2>&1; then
-  echo "Docker is already installed"
-else
+if [ command -v docker &> /dev/null ]; then
   # Downloads docker using the script from the docker website
-  if command -v curl > /dev/null 2>&1; then
+  if [ command -v curl &> /dev/null ]; then
     curl -fsSL https://get.docker.com -o get-docker.sh
     sh ./get-docker.sh --dry-run
     echo "Docker installed"
   else
     echo "Curl is not installed, please install it"
-    exit 1
+    exit
   fi
+else
+ echo "Docker is already installed"
 fi
 
 # Enables and starts docker if systemctl is available, this only works on linux systems that use systemd
-if command -v systemctl > /dev/null 2>&1; then
+if [ command -v systemctl &> /dev/null ]; then
   systemctl enable --now docker
 fi
 
-if ! docker volume inspect dev-env_db_data > /dev/null 2>&1; then
-  docker volume create dev-env_db_data
+if [ ! -f .env ]; then
+  cp .env.example .env
 fi
 
 docker compose up -d --build
+
+echo "Everything's setup"
